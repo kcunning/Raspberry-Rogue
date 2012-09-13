ffa3bea7c64785818e03f703ad7f6a0ec5fd1aa8
Katie Cunningham <kcunningham@ecpkidmk16.coxinc.net>
2012-09-13 09:11:43 -0400

    Added background from my other roguelike

--- trunk/rogue/rogue.py
+++ trunk/rogue/rogue.py
@@ -1,11 +1,17 @@
 import pygame, math, sys, random
+from os.path import abspath, dirname
 from pygame.locals import *
+
+IMG_DIR = dirname(dirname(abspath(__file__))) + "/rogue/media/"
 
 class GameScreen(object):
     def __init__(self):
         self.screen = pygame.display.set_mode((1280, 832))
         self.font = pygame.font.SysFont(None, 48)
         self.small_font = pygame.font.SysFont(None, 20)
+        self.bg = pygame.image.load(IMG_DIR + 'rainbowbg.png')
+        self.screen.blit(self.bg, (0,0))
+        pygame.display.flip()
 
 def main():
     while True:
